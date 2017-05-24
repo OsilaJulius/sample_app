715d6f23656e9a258613db58bbdbb74ddddc12a5
OsilaJulius <osilajules@gmail.com>
2017-05-24 18:58:59 +0800

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ApplicationRecord
 
   before_save { email.downcase! }
 
-  validates :name, presence: true, length: { maximum: 50}
-  validates :email, presence: true, length: { maximum: 255},
-    format: { with: VALID_EMAIL_REGEX  }, uniqueness: { case_sensitive: false}
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX  }, uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 },
+    confirmation: { case_sensitive: false }
+  validates :password_confirmation, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+
   #static page routes
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
@@ -7,4 +9,5 @@ Rails.application.routes.draw do
 
   #User routes
   get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
 end
