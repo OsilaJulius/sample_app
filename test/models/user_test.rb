1b3121bdeb98a51363173df6576fc8dce3f1a956
OsilaJulius <osilajules@gmail.com>
2017-06-02 19:30:14 +0800

    Improve feeds

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -86,4 +86,22 @@ class UserTest < ActiveSupport::TestCase
     railstut.unfollow(archer)
     assert_not railstut.following?(archer)
   end
+
+  test "feed should have the right posts" do
+    railstut = users(:railstut)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert railstut.feed.include?(post_following)
+    end
+    # Posts from self
+    railstut.microposts.each do |post_self|
+      assert railstut.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not railstut.feed.include?(post_unfollowed)
+    end
+  end
 end
