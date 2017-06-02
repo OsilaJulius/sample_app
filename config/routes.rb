9398b0dad01cff20af642b3408f5ac6a630fc2ce
OsilaJulius <osilajules@gmail.com>
2017-06-02 09:59:40 +0800

    Finish user mircopost

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
-  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
 
   #static page routes
   root 'static_pages#home'
